d05e390c2af66dd5722faaf7c8b229b26b621366
Everton Kluczkovski <kluczkovski@hotmail.com>
2019-06-17 14:01:11 +0100

    Setup Project and DBContext

---
+++ WebAPI_Angular/Models/PaymentDetail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI_Angular.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string CarOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string CVV{ get; set; }
+
+        public PaymentDetail()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Car Name {CarOwnerName}, and Number {CardNumber}"; 
+        }
+    }
+}
---
+++ WebAPI_Angular/Models/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_Angular.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) :base(options)
+        {
+        }
+
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set;}
+    }
+}
